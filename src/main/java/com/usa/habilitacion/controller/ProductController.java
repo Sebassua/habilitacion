a8ecfd35e72151ce0e993c5a6e2837072a85a04c
Sebassua <sebastian.suarez2.mt@correo.usa.edu.co>
2021-12-28 23:20:00 -0500

    Update product controller and application.java

--- src/main/java/com/usa/habilitacion/controller/ProductController.java
+++ src/main/java/com/usa/habilitacion/controller/ProductController.java
@@ -56,7 +56,7 @@ public class ProductController {
     public boolean delete(@PathVariable("reference") String reference){
         return productService.delete(reference);
     }
-    @GetMapping("/description/{descripcion}")
+    @GetMapping("/description/{description}")
     public List<Product> findByDescriptionContainingIgnoreCase(@PathVariable("description") String description){
         return productService.findByDescriptionContainingIgnoreCase(description);
     }
